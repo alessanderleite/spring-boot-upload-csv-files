cc56e75136cb44ce1dcbf938e1fc402a4617985c
Alessander <alessander.logistica@gmail.com>
2020-05-19 23:33:11 -0300

    initial commit

---
+++ src/main/java/com/example/app/SpringBootUploadCsvFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadCsvFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadCsvFilesApplication.class, args);
+	}
+
+}
